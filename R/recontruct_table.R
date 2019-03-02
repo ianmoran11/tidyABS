f132cb9d7193d8be39fdf6699d9dfe7cbc447fde
Ian Moran <ian.moran11@gmail.com>
2019-03-03 01:57:59 +1100

    added recontruct_table function

---
+++ R/recontruct_table.R
@@ -0,0 +1,51 @@
+#' recontruct_table
+#'
+#' Reconstructs the table components to structure of oringinal excel file.
+#'
+#' @param table_components object returned by `process_ABS_sheet`
+#'
+#' @export
+
+
+
+recontruct_table <-
+  function(table_components){
+
+    #!#!#!# Use xltabr here
+
+    table_components %>%
+      assemble_table_components() %>%
+      mutate_at(.vars = vars(matches("row_")),
+                .funs = funs( ifelse(is.na(.),
+                                     NA,
+                                     paste(str_pad(as.character(row),width = 5,pad = "0"),
+                                           .,
+                                           sep = "_")))) %>%
+      select(-row,-col,-comment) %>%
+      mutate( cols = paste3(!!!syms(names(.)[str_detect(names(.),"col_")]), sep = "_")) %>%
+      select(-matches("col_"))  %>%
+      mutate(value = as.numeric(value)) %>%
+      spread(cols,value) %>%
+      arrange(!!!syms(names(.)[str_detect(names(.),"row_")])) %>%
+      mutate_at(.vars = vars(matches("row_")),.funs = funs( ifelse(is.na(.), NA, str_remove_all(.,"[0-9]{5}_"))))
+
+  }
+
+#' paste3
+#'
+#' Removes NAs from paste. Taken from https://stackoverflow.com/users/1855677/42 stackoverflow answer.
+#'
+#' @param table_components object returned by `process_ABS_sheet`
+#'
+#' @export
+
+paste3 <- function(...,sep=", ") {
+  L <- list(...)
+  L <- lapply(L,function(x) {x[is.na(x)] <- ""; x})
+  ret <-gsub(paste0("(^",sep,"|",sep,"$)"),"",
+             gsub(paste0(sep,sep),sep,
+                  do.call(paste,c(L,list(sep=sep)))))
+  is.na(ret) <- ret==""
+  ret
+}
+
